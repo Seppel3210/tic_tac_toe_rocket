d77988a73a7beb7c4a15c28665b14ebb2e95c602
Sebastian Widua <seppel3210@gmail.com>
2022-02-07 18:26:15 +0100

    Initial commit

---
+++ src/game.rs
@@ -0,0 +1,100 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Player {
+    X,
+    O,
+}
+
+impl Player {
+    fn next_player(&mut self) {
+        *self = match self {
+            Player::X => Player::O,
+            Player::O => Player::X,
+        }
+    }
+
+    pub fn name(self) -> &'static str {
+        match self {
+            Player::X => "X",
+            Player::O => "O",
+        }
+    }
+}
+
+const WIDTH: usize = 3;
+
+pub struct TicTacToe {
+    field: [Option<Player>; WIDTH * WIDTH],
+    active_player: Player,
+}
+
+impl TicTacToe {
+    pub fn new() -> Self {
+        Self {
+            field: [None; WIDTH * WIDTH],
+            active_player: Player::X,
+        }
+    }
+
+    pub fn place_cell(&mut self, x: u8, y: u8) -> Result<bool, PlacementError> {
+        let position = (x + y * WIDTH as u8) as usize;
+        let cell = self
+            .field
+            .get_mut(position)
+            .ok_or(PlacementError::OutOfBounds)?;
+        if !cell.is_none() {
+            return Err(PlacementError::AlreadyFilled);
+        }
+
+        *cell = Some(self.active_player);
+        let won = self.won();
+        self.active_player.next_player();
+        Ok(won)
+    }
+
+    pub fn won(&self) -> bool {
+        let mut rows = [0; WIDTH];
+        let mut cols = [0; WIDTH];
+        let mut diagonal_0 = 0;
+        let mut diagonal_1 = 0;
+        for (pos, _) in self
+            .field
+            .iter()
+            .enumerate()
+            .filter_map(|(i, player)| player.map(|p| (i, p)))
+            .filter(|(_, player)| player == &self.active_player)
+        {
+            let x = pos % WIDTH;
+            let y = pos / WIDTH;
+            cols[x] += 1;
+            rows[y] += 1;
+            if y == x {
+                diagonal_0 += 1;
+            }
+            if y == 2 - x {
+                diagonal_1 += 1;
+            }
+        }
+        rows.into_iter()
+            .chain(cols)
+            .chain([diagonal_0, diagonal_1])
+            .any(|x| x == 3)
+    }
+
+    pub fn active_player(&self) -> Player {
+        self.active_player
+    }
+}
+
+pub enum PlacementError {
+    OutOfBounds,
+    AlreadyFilled,
+}
+
+impl PlacementError {
+    pub fn message(&self) -> &'static str {
+        match self {
+            PlacementError::OutOfBounds => "position is out of bounds",
+            PlacementError::AlreadyFilled => "cell is already filled",
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+#[macro_use]
+extern crate rocket;
+
+use dashmap::DashMap;
+use rocket::{State, serde::{Deserialize, json::Json}};
+use uuid::Uuid;
+
+mod game;
+
+use game::TicTacToe;
+
+type GameStorage = DashMap<Uuid, TicTacToe>;
+
+#[post("/create")]
+fn create_game(games: &State<GameStorage>) -> String {
+    let game_id = loop {
+        let id = Uuid::new_v4();
+        if !games.contains_key(&id) {
+            break id;
+        }
+    };
+    games.insert(game_id, TicTacToe::new());
+    game_id.to_string()
+}
+
+#[derive(Deserialize)]
+struct MoveInfo {
+    x: u8,
+    y: u8,
+}
+
+#[post("/play/<game_id>", data = "<move_info>")]
+fn place_symbol(
+    game_id: Uuid,
+    move_info: Json<MoveInfo>,
+    games: &State<GameStorage>,
+) -> Result<String, &'static str> {
+    let MoveInfo { x, y } = move_info.0;
+    let (won, active_player) = {
+        let mut board = games.get_mut(&game_id).ok_or("invalid game id")?;
+        let current_player = board.active_player();
+        (board.place_cell(x, y).map_err(|err| err.message())?, current_player)
+    };
+    Ok(if won {
+        games.remove(&game_id);
+        format!("{} won!", active_player.name())
+    } else {
+        String::new()
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![create_game, place_symbol, hello])
+        .manage(GameStorage::new())
+}
